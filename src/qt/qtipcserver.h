73c2fac0d14489dd79f2f23d354830919a59d402
Stefan Macheiner <snidecoin@gmail.com>
2018-04-17 10:57:51 +0200

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define snide-qt message queue name
+#define BITCOINURI_QUEUE_NAME "snideURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
